2013d6387e8f65c07f97f52d1916a96780047928
Bruno Luiz Viana <brunolviana22@hotmail.com>
2026-05-04 18:12:10 -0300

    feat(api): Update EventoEnvolvidoInput to use PapelEnvolvido enum

--- app/src/main/java/br/com/nsfatima/calendario/api/dto/evento/EventoEnvolvidoInput.java
+++ app/src/main/java/br/com/nsfatima/calendario/api/dto/evento/EventoEnvolvidoInput.java
@@ -1,9 +1,10 @@
 package br.com.nsfatima.calendario.api.dto.evento;
 
+import br.com.nsfatima.calendario.domain.type.PapelEnvolvido;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record EventoEnvolvidoInput(
         @NotNull UUID organizacaoId,
-        String papel) {
+        @NotNull PapelEnvolvido papel) {
 }
